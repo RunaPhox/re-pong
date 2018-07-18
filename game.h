66fe0a85c38cade0595ed43a9f9ba1171cfcd028
Gabriel D'Aquino <gabriel.leonardo.d@gmail.com>
2018-07-17 21:57:03 -0400

    Refactored code into new header.

---
+++ game.h
@@ -0,0 +1,62 @@
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+typedef struct {
+	SDL_Window *w;
+	SDL_Renderer *r;
+
+	SDL_Event e;
+	SDL_Rect rct;
+} Game;
+
+Game game;
+
+void init();
+void run();
+void update();
+void draw();
+static void close();
+
+void
+init()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	game.w = SDL_CreateWindow("Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	game.r = SDL_CreateRenderer(game.w, -1, SDL_RENDERER_ACCELERATED);
+}
+
+void
+run()
+{
+	int quit = 0;
+	while (!quit) {
+		while (SDL_PollEvent(&game.e)) {
+			if (game.e.type == SDL_QUIT) {
+				quit = 1;
+			}
+		}
+		update();
+		draw();
+	}
+}
+
+void
+update()
+{
+}
+
+void
+draw()
+{
+	SDL_SetRenderDrawColor(game.r, 0x28, 0x28, 0x28, 0xff);
+	SDL_RenderClear(game.r);
+	SDL_RenderPresent(game.r);
+}
+
+static void
+close()
+{
+	SDL_Quit();
+}
